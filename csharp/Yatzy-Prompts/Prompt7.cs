c5687be1fe7c750aa631914a9d2fb05c74790eff
emily <emily@bacheconsulting.com>
2022-12-13 08:18:08 +0100

    update .net version, improve assertions

--- csharp/Yatzy-Prompts/Prompt7.cs
+++ csharp/Yatzy-Prompts/Prompt7.cs
@@ -7,19 +7,18 @@ public class Prompt7
 {
     // this test case should be added to YatzyScorerTest
     [Test]
-    public void DiceFrequencies_stores_how_many_of_each_dice_number_is_present()
+    public void DiceFrequencies_gives_how_many_of_each_dice_is_present()
     {
-        Assert.AreEqual(new Dictionary<int, int>()
+        CollectionAssert.AreEquivalent(new Dictionary<int, int>()
             {
                 { 1, 0 }, { 2, 5 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 }
             }, 
-            YatzyScorer.DiceFrequencies(2,2,2,2,2));        
-        
-        Assert.AreEqual(new Dictionary<int, int>()
+            YatzyScorer.DiceFrequencies(2,2,2,2,2));
+
+        CollectionAssert.AreEquivalent(new Dictionary<int, int>()
             {
                 { 1, 1 }, { 2, 1 }, { 3, 1 }, { 4, 1 }, { 5, 1 }, { 6, 0 }
-            }, 
-            YatzyScorer.DiceFrequencies(1,2,3,4,5));
-        
+            },
+            YatzyScorer.DiceFrequencies(1, 2, 3, 4, 5));
     }
 }
--- csharp/Yatzy-Prompts/YatzyScorerTest.cs
+++ csharp/Yatzy-Prompts/YatzyScorerTest.cs
@@ -43,18 +43,17 @@ public class YatzyScorerTest
     [Test]
     public void DiceFrequencies_gives_how_many_of_each_dice_number_is_present()
     {
-        Assert.AreEqual(new Dictionary<int, int>()
+        CollectionAssert.AreEquivalent(new Dictionary<int, int>()
             {
                 { 1, 0 }, { 2, 5 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 }
             }, 
-            YatzyScorer.DiceFrequencies(2,2,2,2,2));        
-        
-        Assert.AreEqual(new Dictionary<int, int>()
+            YatzyScorer.DiceFrequencies(2,2,2,2,2));
+
+        CollectionAssert.AreEquivalent(new Dictionary<int, int>()
             {
                 { 1, 1 }, { 2, 1 }, { 3, 1 }, { 4, 1 }, { 5, 1 }, { 6, 0 }
-            }, 
-            YatzyScorer.DiceFrequencies(1,2,3,4,5));
-        
+            },
+            YatzyScorer.DiceFrequencies(1, 2, 3, 4, 5));
     }
     
     [Test]
